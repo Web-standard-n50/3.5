72ba4bab78aeeb4e94d6b7f9e0b3990af0930b44
MJurayev <jurayevmansurbek667@gmail.com>
2022-08-16 10:03:00 +0500

    initial commmit

---
+++ main.js
@@ -0,0 +1,137 @@
+// console.log("Hello World");
+
+// var firstname = 12;
+
+// var isAdmin = false;
+var username;
+var lastname;
+var car;
+
+
+// a
+// b
+
+// senderAccount
+// recieverAccount
+
+// camelCase
+var senderAccountBalance;
+
+// PascalCase
+var AccountBalance;
+
+// snake_case
+var sender_account_balance;
+
+// kebab-case
+// var sender-account-balance not supported in JS
+
+// bu funksiya sanani formatlab beradi
+// @params date:12.03.2022
+
+/*
+ * Author:
+ * @params
+ *
+ *
+ *
+ *
+ */
+
+
+// var text = "Shaxmatchilarimizga trailblazer avtomobili sovg'a qilindi!!!";
+// var typeOfText = typeof text;
+
+// console.log(typeOfText);
+
+var year = 2022;
+year = 12;
+// console.log(year);
+// console.log(typeof year);
+
+// var obj = {
+//     firstname: "John",
+//     lastname: "Doe",
+//     age: 23
+// };
+
+// obj.age = 30;
+
+// console.log(obj);
+
+// console.log("Ismingiz:", obj.firstname);
+// console.log("Ismingiz:", obj.lastname);
+// console.log("Yoshingiz:", obj.age);
+
+
+// var nullVarialble = null;
+// console.log(typeof nullVarialble);
+
+// var a;
+// console.log(typeof a);
+// var a = 12;
+// var b = "12";
+// var isEqual = (a == b);
+// console.log(isEqual)
+
+// var isTrue = 0 > 1;
+// console.log(isTrue)
+
+// var sym1 = Symbol();
+// var sym2 = Symbol();
+
+// console.log(sym1 === sym2)
+
+
+// var str1 = 'Hello';
+// var str2 = 'hello';
+
+// console.log(str1 == str2);
+
+// var firstname = '\"Toshkent\" O\'zbekistonni poytaxti';
+// console.log(firstname);
+
+var lastname = "Hello"
+var lastName = "Salom"
+
+// console.log(lastname);
+// console.log(lastName);
+
+// var var ;
+// var for, if, switch, case, const, else,
+
+// var 12asd;
+// var _12asd;
+
+// var str2 = `Hello World`;
+// console.log(str2);
+
+// var lastname = "Qodirov";
+// var firstname = "Toshpo'lat";
+
+// console.log(firstname + ' ' + lastname);
+
+// console.log(`Ismi: ${firstname} ${lastname}`)
+
+
+var user = {}
+
+// user.firstname = prompt("Ismingizni kiriting:", "John")
+// user.lastname = prompt("Familiyangizni kiriting:", "Doe");
+// user.age = prompt("Yoshingizni kiriting:", 20);
+
+
+// alert(`Assalomu alaykum, ${user.firstname} ${user.lastname}. Sizning yoshingiz ${user.age}`);
+// console.log(user)
+// window.document
+// window.navigator
+// window.alert
+// window.prompt
+
+// alert("Ma'lumotlarni ulashganingiz tashakkur!!!")
+var text = "Hello \n WOrld \t Salom \n  Dunyo";
+console.log(text)
+
+console.log(12 + 13);
+console.log(12 - "14");
+console.log("14" - "2");
